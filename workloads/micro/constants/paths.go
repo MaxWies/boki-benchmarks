477aaab165dec35619412b2fca1bc97bfad3ce55
Maximilian Wiesholler <maximilian.georg.wiesholler@huawei.com>
2022-03-01 09:41:27 +0100

    path change

--- workloads/micro/constants/paths.go
+++ workloads/micro/constants/paths.go
@@ -2,5 +2,5 @@ package constants
 
 const (
 	BASE_PATH_ENGINE_BOKI_BENCHMARK string = "/tmp/boki/output/benchmark"
-	BASE_PATH_CLIENT_BOKI_BENCHMARK string = "/tmp/benchmark"
+	BASE_PATH_CLIENT_BOKI_BENCHMARK string = "/tmp/boki/output/benchmark"
 )
