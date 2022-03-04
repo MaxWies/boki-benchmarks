fb13efe0b0d8826ea160d2c20edb713d8f721cdc
Maximilian Wiesholler <maximilian.georg.wiesholler@huawei.com>
2022-03-04 16:57:46 +0100

    append-read-benchmark

---
+++ experiments/micro/append-and-read/run_all.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+BASE_DIR=`realpath $(dirname $0)`
+ROOT_DIR=`realpath $BASE_DIR/../../..`
+CONFIG_DIR=`realpath $ROOT_DIR/..`
+
+BENCHMARK=$(basename "$PWD")
+HELPER_SCRIPT=$ROOT_DIR/scripts/exp_helper
+
+BOKI_SETTING="eng1-st1-seq3"
+CONFIGURATION=$BENCHMARK-$BOKI_SETTING
+echo "Run benchmarks for $CONFIGURATION"
+cp $CONFIG_DIR/config_$CONFIGURATION.json $BASE_DIR/config.json
+cp $BASE_DIR/$BOKI_SETTING/docker-compose.yml $BASE_DIR
+$HELPER_SCRIPT start-machines --base-dir=$BASE_DIR
+$BASE_DIR/run_once.sh $BOKI_SETTING $BENCHMARK 320 20 1024 4
+# $BASE_DIR/run_once.sh $BOKI_SETTING $BENCHMARK 640 20 1024
+# $BASE_DIR/run_once.sh $BOKI_SETTING $BENCHMARK 1280 20 1024
+# $BASE_DIR/run_once.sh $BOKI_SETTING $BENCHMARK 2560 20 1024
+
+# BOKI_SETTING="eng4-st1-seq3"
+# CONFIGURATION=$BENCHMARK-$BOKI_SETTING
+# echo "Run benchmarks for $CONFIGURATION"
+# cp $CONFIG_DIR/config_$CONFIGURATION.json $BASE_DIR/config.json
+# cp $BASE_DIR/$BOKI_SETTING/docker-compose.yml $BASE_DIR
+# $HELPER_SCRIPT start-machines --base-dir=$BASE_DIR
+# $BASE_DIR/run_once.sh $BOKI_SETTING $BENCHMARK 320 20 1024
+# $BASE_DIR/run_once.sh $BOKI_SETTING $BENCHMARK 640 20 1024
+# $BASE_DIR/run_once.sh $BOKI_SETTING $BENCHMARK 1280 20 1024
+# $BASE_DIR/run_once.sh $BOKI_SETTING $BENCHMARK 2560 20 1024
