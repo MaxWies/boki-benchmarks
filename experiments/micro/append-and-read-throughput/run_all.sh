dd02c6f963d5a0d1e7f73104549dc7233716fc9b
Maximilian Wiesholler <maximilian.georg.wiesholler@huawei.com>
2022-03-08 11:27:16 +0100

    fix

--- experiments/micro/append-and-read-throughput/run_all.sh
+++ experiments/micro/append-and-read-throughput/run_all.sh
@@ -4,8 +4,6 @@ ROOT_DIR=`realpath $BASE_DIR/../../..`
 
 HELPER_SCRIPT=$ROOT_DIR/scripts/exp_helper
 
-cp $ROOT_DIR/scripts/run_once.sh run_once.sh
-
 echo "Run benchmarks for engine:1 storage:1 sequencer:3"
 cp $BASE_DIR/machines_eng1-st1-seq3.json $BASE_DIR/machines.json
 $HELPER_SCRIPT start-machines --base-dir=$BASE_DIR
