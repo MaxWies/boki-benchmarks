c377d293a72ab1a4c7bbf9d7dd3d76ede4751715
Maximilian Wiesholler <maximilian.georg.wiesholler@huawei.com>
2022-03-25 21:09:48 +0100

    single-index specs

---
+++ experiments/micro/single-index/run_all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+BASE_DIR=`realpath $(dirname $0)`
+ROOT_DIR=`realpath $BASE_DIR/../../..`
+CONTROLLER_SPEC_DIR=$ROOT_DIR/controller-spec
+
+HELPER_SCRIPT=$ROOT_DIR/scripts/exp_helper
+
+cp $BASE_DIR/machines_eng2-st1-seq3_engh1.json $BASE_DIR/machines.json
+$HELPER_SCRIPT start-machines --base-dir=$BASE_DIR
+./run_once.sh $CONTROLLER_SPEC_DIR/eng1-ei1-st1-seq3-ir1-ur1-mr3.json $BASE_DIR/specs/exp-cf8.json
