82984c7b1bc32da6dda669abc3e515300570e584
Maximilian Wiesholler <maximilian.georg.wiesholler@huawei.com>
2022-04-03 22:08:56 +0200

    fix scripts

--- scripts/resource_usage.sh
+++ scripts/resource_usage.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-PNAME="$1"
+PID="$1"
 SAMPLE_RATE="$2"
 SAMPLES="$3"
 LOG_FILE="$4"
-PID=$(pidof ${PNAME})
+PNAME=$(tr -d '\0' </proc/$PID/cmdline)
 
 top -b -d $SAMPLE_RATE -n $SAMPLES -p $PID | awk \
     -v cpuLog="$LOG_FILE" -v pid="$PID" -v pname="$PNAME" '
     /^top -/{time = $3}
-    $1+0>0 {printf "%s,%s,%s,%s,%d,%d\n", \
+    $1+0>0 {printf "%s,%s,%s,%s,%.1f,%.1f\n", \
             strftime("%Y-%m-%d"), time, pname, pid, $9, $10 > cpuLog
             fflush(cpuLog)}'
