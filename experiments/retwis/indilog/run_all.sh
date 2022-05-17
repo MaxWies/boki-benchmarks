d79f7444b894c61b91e4b1759dfa45815f91cb00
Maximilian Wiesholler <maximilian.georg.wiesholler@huawei.com>
2022-05-17 15:15:44 +0200

    new benchmarks for transaction lib

---
+++ experiments/retwis/indilog/run_all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+BASE_DIR=`realpath $(dirname $0)`
+ROOT_DIR=`realpath $BASE_DIR/../../..`
+CONTROLLER_SPEC_DIR=$ROOT_DIR/controller-spec
+
+HELPER_SCRIPT=$ROOT_DIR/scripts/exp_helper
+
+echo "Run benchmarks for engine:2 storage:2 sequencer:3 index-shards:2 index-replication:1"
+cp $BASE_DIR/machines_eng2-st2-seq3-ix2.json $BASE_DIR/machines.json
+$HELPER_SCRIPT start-machines --base-dir=$BASE_DIR
+./run_once.sh $CONTROLLER_SPEC_DIR/ei2-st2-seq3-ix2-is2-ir1-ur1-mr3-ssmx4.json $BASE_DIR/specs/exp-cf24.json
