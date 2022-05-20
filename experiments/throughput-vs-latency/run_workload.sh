4c3b07d95169efbe4ba9a667772dcd0d33c8e57e
Maximilian Wiesholler <maximilian.georg.wiesholler@huawei.com>
2022-05-20 13:39:58 +0200

    workload for boki

--- experiments/throughput-vs-latency/run_workload.sh
+++ experiments/throughput-vs-latency/run_workload.sh
@@ -20,15 +20,17 @@ COLLECTION_DIR=$BASE_DIR/results/collection
 rm -rf $COLLECTION_DIR
 mkdir -p $COLLECTION_DIR $COLLECTION_DIR/indilog $COLLECTION_DIR/boki
 
+# Boki
+cp $BASE_DIR/machines_eng1-st1-seq3.json $BASE_DIR/machines.json
+$HELPER_SCRIPT start-machines --base-dir=$BASE_DIR
+./run_build.sh boki $CONTROLLER_SPEC_DIR/boki/eng1-st1-seq3-ir1-ur1-mr3.json $BASE_DIR/specs/exp-cf1.json
+./run_build.sh boki $CONTROLLER_SPEC_DIR/boki/eng1-st1-seq3-ir1-ur1-mr3.json $BASE_DIR/specs/exp-cf8.json
+
 # Indilog
 cp $BASE_DIR/machines_eng1-st1-seq3-ix1.json $BASE_DIR/machines.json
 $HELPER_SCRIPT start-machines --base-dir=$BASE_DIR
-./run_build.sh indilog $CONTROLLER_SPEC_DIR/indilog/eng1-st1-seq3-ix1-is1-ir1-ur1-mr3-ssmx4.json $BASE_DIR/specs/exp-cf1.json 
-
-# Boki
-#cp $BASE_DIR/machines_eng1-st1-seq3.json $BASE_DIR/machines.json
-#$HELPER_SCRIPT start-machines --base-dir=$BASE_DIR
-#./run_build.sh boki $CONTROLLER_SPEC_DIR/boki/eng1-st1-seq3-ir1-ur1-mr3.json $BASE_DIR/specs/exp-cf1.json
+./run_build.sh indilog $CONTROLLER_SPEC_DIR/indilog/eng1-st1-seq3-ix1-is1-ir1-ur1-mr3-ssmx4.json $BASE_DIR/specs/exp-cf1.json
+./run_build.sh indilog $CONTROLLER_SPEC_DIR/indilog/eng1-st1-seq3-ix1-is1-ir1-ur1-mr3-ssmx4.json $BASE_DIR/specs/exp-cf8.json 
 
 # Benchmark collected csv file
 $BENCHMARK_SCRIPT throughput-vs-latency-plot --file=$COLLECTION_DIR/throughput-vs-latency.csv --result-file=$END_RESULT_DIR/throughput-vs-latency.png
