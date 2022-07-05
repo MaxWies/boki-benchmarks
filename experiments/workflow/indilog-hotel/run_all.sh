d41da51ffd883d4b695fe68e9d0b9f7a7ccb6e9c
Maximilian Wiesholler <maximilian.georg.wiesholler@huawei.com>
2022-07-06 00:39:36 +0800

    improve workflow

--- experiments/workflow/indilog-hotel/run_all.sh
+++ experiments/workflow/indilog-hotel/run_all.sh
@@ -9,31 +9,20 @@ BENCHMARK_SCRIPT=$BASE_DIR/summarize_benchmarks
 BENCHMARK_HELPER_SCRIPT=$ROOT_DIR/scripts/benchmark_helper
 
 RESULT_DIR=$BASE_DIR/results
-# rm -rf $RESULT_DIR
+rm -rf $RESULT_DIR
 mkdir -p \
     $RESULT_DIR \
     $RESULT_DIR/boki \
     $RESULT_DIR/indilog \
-    $RESULT_DIR/indilog-min-completion \
-    $RESULT_DIR/indilog-test
+    $RESULT_DIR/indilog-min-completion
 
-# $HELPER_SCRIPT reboot-machines --base-dir=$MACHINE_SPEC_DIR
-# sleep 90
+$HELPER_SCRIPT reboot-machines --base-dir=$MACHINE_SPEC_DIR
+sleep 90
 
-# # # Boki-Local
-# cp $MACHINE_SPEC_DIR/boki/machines_eng4-st4-seq3-dy1.json $BASE_DIR/machines.json
-# $HELPER_SCRIPT start-machines --base-dir=$BASE_DIR
-# ./run_build.sh boki $CONTROLLER_SPEC_DIR/boki/eng4-st4-seq3-ir4-ur1-mr3.json $BASE_DIR/specs/exp-cf8.json
-
-
-# $HELPER_SCRIPT reboot-machines --base-dir=$MACHINE_SPEC_DIR
-# sleep 90
-
-# # Indilog
-# cp $MACHINE_SPEC_DIR/indilog/machines_eng4-st4-seq3-ix2-agg1-dy1.json $BASE_DIR/machines.json
-# $HELPER_SCRIPT start-machines --base-dir=$BASE_DIR
-# # ./run_build.sh indilog $CONTROLLER_SPEC_DIR/indilog/eng4-st4-seq3-ix2-agg1-dy1-is2-ir1-ur1-mr3-ssmx4.json $BASE_DIR/specs/exp-cf8.json
-# ./run_build.sh indilog-min-completion $CONTROLLER_SPEC_DIR/indilog/eng4-st4-seq3-ix2-agg1-dy1-is2-ir1-ur1-mr3-ssmx4.json $BASE_DIR/specs/exp-cf8.json
+# Boki-Local
+cp $MACHINE_SPEC_DIR/boki/machines_eng4-st4-seq3-dy1.json $BASE_DIR/machines.json
+$HELPER_SCRIPT start-machines --base-dir=$BASE_DIR
+./run_build.sh boki $CONTROLLER_SPEC_DIR/boki/eng4-st4-seq3-dy1-ir4-ur1-mr3.json $BASE_DIR/specs/exp-cf8.json
 
 $HELPER_SCRIPT reboot-machines --base-dir=$MACHINE_SPEC_DIR
 sleep 90
@@ -41,14 +30,15 @@ sleep 90
 # Indilog
 cp $MACHINE_SPEC_DIR/indilog/machines_eng4-st4-seq3-ix2-agg1-dy1.json $BASE_DIR/machines.json
 $HELPER_SCRIPT start-machines --base-dir=$BASE_DIR
-./run_build.sh indilog-test $CONTROLLER_SPEC_DIR/indilog/eng4-st4-seq3-ix2-agg1-dy1-is2-ir1-ur1-mr3-ssmx4.json $BASE_DIR/specs/exp-cf8.json
+./run_build.sh indilog $CONTROLLER_SPEC_DIR/indilog/eng4-st4-seq3-ix2-agg1-dy1-is2-ir1-ur1-mr3-ssmx4.json $BASE_DIR/specs/exp-cf8.json
+./run_build.sh indilog-min-completion $CONTROLLER_SPEC_DIR/indilog/eng4-st4-seq3-ix2-agg1-dy1-is2-ir1-ur1-mr3-ssmx4.json $BASE_DIR/specs/exp-cf8.json
 
 # Plotting
-# for FILE_TYPE in "pdf" "png";
-# do
-#     for SLOG in boki indilog indilog-min-completion; do
-#         $BENCHMARK_HELPER_SCRIPT generate-operation-statistics-plot \
-#             --file=$RESULT_DIR/$SLOG/operations.csv \
-#             --result-file=$RESULT_DIR/$SLOG/operations.$FILE_TYPE
-#     done
-# done
+for FILE_TYPE in "pdf" "png";
+do
+    for SLOG in boki indilog indilog-min-completion; do
+        $BENCHMARK_HELPER_SCRIPT generate-operation-statistics-plot \
+            --file=$RESULT_DIR/$SLOG/operations.csv \
+            --result-file=$RESULT_DIR/$SLOG/operations.$FILE_TYPE
+    done
+done
