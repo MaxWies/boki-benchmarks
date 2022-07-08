60c76b7fa75185ac2037b55bf1b0dcb617e18344
Maximilian Wiesholler <maximilian.georg.wiesholler@huawei.com>
2022-07-08 18:02:34 +0200

    delete unused script

--- scripts/bandwidth.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-NETWORK_INTERFACE=$1
-DURATION=$2
-OUTPUT_FILE=$3
-
-bash -ic "{ /usr/sbin/nethogs -t $NETWORK_INTERFACE &> $OUTPUT_FILE; \
-    kill 0; } | { sleep $DURATION; \
-    kill 0; }" 3>&1 2>/dev/null
