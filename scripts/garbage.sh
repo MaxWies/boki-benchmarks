fe47ec309825fcb50a9b1ae4851f837031a6b722
Maximilian Wiesholler <maximilian.georg.wiesholler@huawei.com>
2022-07-08 18:02:05 +0200

    delete unused scripts

--- scripts/garbage.sh
+++
@@ -1,7 +0,0 @@
-WORKING_DIR=$1
-
-rm $WORKING_DIR/config.json
-rm $WORKING_DIR/docker-compose.yml
-rm $WORKING_DIR/docker-compose-generated.yml
-rm $WORKING_DIR/machines.json
-rm $WORKING_DIR/nightcore_config.json
